f8849918e507d0bd404c155a38d18e005729f947
GORKEM VARIS <91727142+Gorkem-Varis@users.noreply.github.com>
2021-10-01 10:16:55 +0000

    I take inputs to learn x and y. Then I do some mathematical calculations.

---
+++ main.py
@@ -0,0 +1,13 @@
+import math
+
+x = int(input("Please enter number x:"))
+y = int(input("Please enter number y:"))
+
+result = x**y 
+print("x**y=", result)
+
+a = math.log2(int(x))
+print("a=", a)
+
+
+print("75916")
